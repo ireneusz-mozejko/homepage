2d5a01c07f94427cb37ddc58c5309108b17b50a9
Ireneusz Mozejko <imozejko@gmail.com>
2022-08-21 20:34:52 +0200

    Added contact page

--- main.js
+++ main.js
@@ -7,6 +7,30 @@ const heading = document.querySelector('.main__heading--js');
 
 heading.innerHTML = `Elo Elo 3 - 5 - 0! Jestem ${firstName} i mam ${age} lat.`;
 
-const emptyParagraph = document.querySelector('.week-summary__description--js');
 
-emptyParagraph.innerHTML = 'Nawet uzupelniłem treść'
+
+function calculate(myNumber){
+    console.log(`Dostałem ${myNumber}`);
+    return `wynik ${myNumber*7}`;
+}
+
+const myReturn = calculate(age);
+
+console.log(myReturn);
+
+function createContent(querySelectorContent, content){
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+
+}
+createContent('.week-summary__description--js','Nawet uzupelniłem treść!');
+
+
+
+const greet = (age, firstName) => {
+    console.log(
+        `Witaj w świecie frontendu, nazywma się ${firstName} i mam ${age} lata.`
+    )
+}
+
+greet(35, 'Irek');
